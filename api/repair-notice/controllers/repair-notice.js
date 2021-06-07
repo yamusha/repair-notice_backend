3a3d3e73e45936cdb22be9b167778ccf6b5e97b6
iFarm <mhukdham@gmail.com>
2021-06-07 12:43:15 +0700

    line notify

--- api/repair-notice/controllers/repair-notice.js
+++ api/repair-notice/controllers/repair-notice.js
@@ -1,8 +1,44 @@
-'use strict';
-
+var request = require("request");
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async line(ctx) {
+    var token = `VjCC1gAJfClc91mkbI15IAjtPzaot4n24L8eqgwiWAb`;
+
+    const { id } = ctx.params;
+    console.log(id);
+    // console.log(ctx.req);
+    if (id) {
+      var message = `โรงเรียน${id} แจ้งซ่อมใหม่ในระบบ`;
+      console.log(message);
+      request(
+        {
+          method: "POST",
+          uri: "https://notify-api.line.me/api/notify",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          auth: {
+            bearer: token,
+          },
+          form: {
+            message: message,
+          },
+        },
+        (err, httpResponse, body) => {
+          if (err) {
+            console.log(err);
+          } else {
+            return { message: "a" };
+          }
+        }
+      );
+    }
+  },
+  async findme(ctx) {
+    console.log("a");
+  },
+};
